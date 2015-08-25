4346c0602b16764a7b7514c3a6a492be2a0c80b4
Jammy Du <jammydu@qq.com>
2015-08-25 21:59:09 +0800

    复习语法

---
+++ practice/src/com/test/test.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int age=12;
+		if(age<=12){
+			System.out.print("上小学");
+		}else{
+			System.out.print("上初中");
+		}
+	}
+}
+	
+
+	
+
